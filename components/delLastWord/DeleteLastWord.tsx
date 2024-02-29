8d2de54abc1f84c6aa7693a957481e4d7fe033cc
Paula Cano <paulacanoroman@gmail.com>
2024-02-29 21:01:04 +0100

    feat: del last word with typed, use ternary operator

--- components/delLastWord/DeleteLastWord.tsx
+++ components/delLastWord/DeleteLastWord.tsx
@@ -3,19 +3,22 @@ import React, { useState } from 'react'
 const DeleteLastWord = () => {
 
     const [inputUser, setInputUser] = useState<string>()
-    const [newSentence, setNewSentence] = useState<string[]>()
+    const [newSentence, setNewSentence] = useState<string>()
 
     const handleDelWord = ()=>{
-        const wordByWord = inputUser?.split(" ")
-        const onlyLastWord = wordByWord?.splice(0,wordByWord.length-1)
-        console.log(onlyLastWord)
+        const wordByWord: string[] | undefined= inputUser?.split(" ")
+        const delLastWord: string[]= wordByWord? wordByWord.slice(0,wordByWord.length-1) : []; // OPERADOR TERNARIO: Comprueba si wordByWord tiene un valor diferente de undefined, si es true ejecuta el slice sino devuelve un array vacio
+        const newSentence: string = delLastWord.join(" ")
+        setNewSentence(newSentence)
+        setInputUser("")
     }
-
+    
   return (
     <div>
         <h3>Modifica la frase eliminando la última palabra</h3>
         <input onChange={(e)=>{setInputUser(e.target.value)}} value={inputUser}></input>
         <button onClick={()=>{handleDelWord()}}>modificar</button>
+        <h4>{newSentence}</h4>
     </div>
   )
 }
