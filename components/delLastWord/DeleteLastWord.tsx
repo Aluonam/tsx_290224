923d54a4cd4c1a5d6c7d20f654886c6e94008adb
Paula Cano <paulacanoroman@gmail.com>
2024-02-29 20:44:11 +0100

    feat: delete last word

--- components/delLastWord/DeleteLastWord.tsx
+++ components/delLastWord/DeleteLastWord.tsx
@@ -5,12 +5,17 @@ const DeleteLastWord = () => {
     const [inputUser, setInputUser] = useState<string>()
     const [newSentence, setNewSentence] = useState<string[]>()
 
+    const handleDelWord = ()=>{
+        const wordByWord = inputUser?.split(" ")
+        const onlyLastWord = wordByWord?.splice(0,wordByWord.length-1)
+        console.log(onlyLastWord)
+    }
 
   return (
     <div>
         <h3>Modifica la frase eliminando la última palabra</h3>
         <input onChange={(e)=>{setInputUser(e.target.value)}} value={inputUser}></input>
-        <button>modificar</button>
+        <button onClick={()=>{handleDelWord()}}>modificar</button>
     </div>
   )
 }
