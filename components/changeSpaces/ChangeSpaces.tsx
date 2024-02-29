314f25c8afa4273d81d26ef6a3693a30ed56f97a
Paula Cano <paulacanoroman@gmail.com>
2024-02-29 21:33:23 +0100

    feat: change space with split and join

--- components/changeSpaces/ChangeSpaces.tsx
+++ components/changeSpaces/ChangeSpaces.tsx
@@ -3,12 +3,21 @@ import React, { useState } from 'react'
 const ChangeSpaces = () => {
 
     const [userSentence, setUserSentence] = useState<string>()
+    const [newSentence, setNewSentence] = useState<string>()
+
+    const changeSpace = ()=>{
+        const separate = userSentence?.split(" ")
+        const union = separate?.join("_")
+        setNewSentence(union)
+    }
 
   return (
     <div>
         <h3>Cambia los espacios de la frase por _</h3>
         <input onChange={(e)=>{setUserSentence(e.target.value)}} value={userSentence}></input>
-        <button onClick={()=>{console.log(userSentence)}}>Cambiar</button>
+        <button onClick={()=>{changeSpace()}}>Cambiar</button>
+        <h4>{newSentence}</h4>
+        
     </div>
   )
 }
