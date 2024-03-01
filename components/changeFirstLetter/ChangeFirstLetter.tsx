b117b0c283059774474b59d1a7b3c03ce35170f9
Paula Cano <paulacanoroman@gmail.com>
2024-03-01 11:14:31 +0100

    feat: skeleton useState and input

--- components/changeFirstLetter/ChangeFirstLetter.tsx
+++ components/changeFirstLetter/ChangeFirstLetter.tsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const ChangeFirstLetter = () => {
+
+    const [userSentence, setUserSentence] = useState<string | undefined>()
+    const [newSentence, setNewSentence] = useState<string[]>()
   return (
-    <div>ChangeFirstLetter</div>
+    <div>
+        <h3>Pon la primera letra de cada palabra en mayúsculas</h3>
+        <input onChange={(e)=>{e.target.value}}></input>
+        <button onClick={()=>{}}>modificar</button>
+    </div>
   )
 }
 
