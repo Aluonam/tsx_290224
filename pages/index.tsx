c181f64f3080d43e2e6c39e6be2df2d902dbcb4d
Paula Cano <paulacanoroman@gmail.com>
2024-03-01 11:07:05 +0100

    feat: new component import

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 import DeleteLastWord from "@/components/delLastWord/DeleteLastWord";
 import ChangeSpaces from "@/components/changeSpaces/ChangeSpaces";
+import ChangeFirstLetter from "@/components/changeFirstLetter/ChangeFirstLetter";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,7 @@ export default function Home() {
       <main className={`${styles.main} ${inter.className}`}>
         <DeleteLastWord></DeleteLastWord>
         <ChangeSpaces></ChangeSpaces>
+        <ChangeFirstLetter></ChangeFirstLetter>
       </main>
     </>
   );
