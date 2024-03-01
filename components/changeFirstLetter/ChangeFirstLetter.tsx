03515fe9be42c1037b5a35318fe27603c90c5f5c
Paula Cano <paulacanoroman@gmail.com>
2024-03-01 11:18:52 +0100

    feaT: function return first letter

--- components/changeFirstLetter/ChangeFirstLetter.tsx
+++ components/changeFirstLetter/ChangeFirstLetter.tsx
@@ -4,11 +4,22 @@ const ChangeFirstLetter = () => {
 
     const [userSentence, setUserSentence] = useState<string | undefined>()
     const [newSentence, setNewSentence] = useState<string[]>()
+
+    const handleChangeFirstCharacter = ()=>{
+        const arrSentence = userSentence?.split(" ");
+        const newArr = arrSentence?.map((oneWord)=>{
+            const letters = oneWord.split("")
+            const firstLetter = letters.at(0)
+            return(firstLetter)
+        })
+        console.log(newArr)
+    }
+
   return (
     <div>
         <h3>Pon la primera letra de cada palabra en mayúsculas</h3>
-        <input onChange={(e)=>{e.target.value}}></input>
-        <button onClick={()=>{}}>modificar</button>
+        <input onChange={(e)=>{setUserSentence(e.target.value)}} value={userSentence}></input>
+        <button onClick={()=>{handleChangeFirstCharacter()}}>modificar</button>
     </div>
   )
 }
